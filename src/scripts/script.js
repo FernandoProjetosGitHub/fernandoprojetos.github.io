ba96c5ddb589a37899e4e0a23ce8b498dac44721
FernandoProjetosGitHub <fernandoaugustoprojetos@gmail.com>
2025-08-12 17:52:31 -0300

    feat: Finalizado projeto

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,6 @@
 
 const toggleTheme = document.getElementById('toggleTheme');
-const rootHtml = document.documentElement
+const rootHtml = document.documentElement;
 
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
@@ -11,4 +11,23 @@ function changeTheme() {
     toggleTheme.classList.toggle("bi-moon-stars");
 }
 
-toggleTheme.addEventListener("click", changeTheme)
+toggleTheme.addEventListener("click", changeTheme);
+
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
+
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
